a701685a7df1efec8915efcf61f2428d40e5c7e3
faiz-webdev <faijuddin@tothenew.com>
2023-06-26 12:38:57 +0530

    error

--- apps/reservation/src/reservation.service.ts
+++ apps/reservation/src/reservation.service.ts
@@ -7,30 +7,30 @@ import { ReservationRepository } from './reservation.repository';
 export class ReservationService {
   constructor(private readonly reservationRepository: ReservationRepository) {}
 
-  create(createReservationDto: CreateReservationDto) {
+  async create(createReservationDto: CreateReservationDto, userId: string) {
     return this.reservationRepository.create({
       ...createReservationDto,
       timestamp: new Date(),
-      userId: '123',
+      userId,
     });
   }
 
-  findAll() {
+  async findAll() {
     return this.reservationRepository.findAll({});
   }
 
-  findOne(_id: string) {
+  async findOne(_id: string) {
     return this.reservationRepository.findOne({ _id });
   }
 
-  update(_id: string, updateReservationDto: UpdateReservationDto) {
+  async update(_id: string, updateReservationDto: UpdateReservationDto) {
     return this.reservationRepository.findOneAndUpdate(
       { _id },
       { $set: updateReservationDto },
     );
   }
 
-  remove(_id: string) {
+  async remove(_id: string) {
     return this.reservationRepository.findOneAndDelete({ _id });
   }
 }
